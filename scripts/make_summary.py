fed04ad4d662b611dd832b78d928df84c7fe4567
millingermarkus <markus.millinger@chalmers.se>
2022-12-20 12:18:24 +0100

    Added global constraints to summary output

--- scripts/make_summary.py
+++ scripts/make_summary.py
@@ -358,6 +358,10 @@ def calculate_metrics(n, label, metrics):
 
     if "CO2Limit" in n.global_constraints.index:
         metrics.at["co2_shadow", label] = n.global_constraints.at["CO2Limit", "mu"]
+    if "co2_sequestration_limit" in n.global_constraints.index:
+        metrics.at["co2_storage_shadow", label] = n.global_constraints.at["co2_sequestration_limit", "mu"]
+    if "lv_limit" in n.global_constraints.index:
+        metrics.at["line_volume_limit", label] = n.global_constraints.at["lv_limit", "mu"]
 
     return metrics
 
